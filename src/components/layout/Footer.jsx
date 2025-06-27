04fdd7d91983782039193f21a756a89fcaf21f58
Abulfeedy <abdulsamadaliyu96@gmail.com>
2025-06-27 18:55:32 +0100

    added a page not found

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <p className='text-xs text-primary text-center mt-6'>
+    <p className='bg-[#f5f5f5] dark:bg-zinc-800  text-xs text-primary font-medium text-center pt-10 pb-4'>
       Powered by{" "}
       <a
         href='https://remotive.com'
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+      {" "}
+      Page Not Found
+    </div>
+  );
 };
 
 export default NotFound;
